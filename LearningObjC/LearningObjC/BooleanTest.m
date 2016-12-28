b634a9b0dd6d39e0520c2adee0aa0625df2c426d
di wu <wudi850620@hotmail.com>
2016-12-29 01:59:54 +1100

    Boolean on Objective C

---
+++ LearningObjC/LearningObjC/BooleanTest.m
@@ -0,0 +1,11 @@
+//
+//  BooleanTest.m
+//  LearningObjC
+//
+//  Created by wu di on 16/12/28.
+//  Copyright © 2016年 wu di. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
---
+++ LearningObjC/LearningObjC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  LearningObjC
+//
+//  Created by wu di on 16/12/28.
+//  Copyright © 2016年 wu di. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
