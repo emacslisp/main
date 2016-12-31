a92124a040979f4298a760e61753dc4b164b8236
emacslisp <wudi850620@hotmail.com>
2017-01-01 02:26:49 +1100

    swift and array

---
+++ LearningSwift/LearningSwift/ArrayAndHashMap.swift
@@ -0,0 +1,31 @@
+//
+//  ArrayAndHashMap.swift
+//  LearningSwift
+//
+//  Created by wu di on 16/12/31.
+//  Copyright © 2016年 wu di. All rights reserved.
+//
+
+import Foundation
+
+
+class ArrayAndHashMap
+{
+    func ArrayTest()
+    {
+        
+        var someInts = [Int]();
+        
+        print("someInts is of type [int] with \(someInts.count) items.");
+        
+        someInts.append(3);
+        
+        
+        
+    }
+    
+    static func main()
+    {
+        
+    }
+}
