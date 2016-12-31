f255dc9fca4a61de33c9d0527b1c1e72311638c3
di wu <wudi850620@hotmail.com>
2016-12-31 14:56:29 +1100

    add swift beginning project

---
+++ LearningSwift/LearningSwift/BooleanTest.swift
@@ -0,0 +1,26 @@
+//
+//  BooleanTest.swift
+//  LearningSwift
+//
+//  Created by wu di on 16/12/31.
+//  Copyright © 2016年 wu di. All rights reserved.
+//
+
+import Foundation
+
+//@example: swift - class static method and print boolean
+class BooleanTest
+{
+    func booleanTest()
+    {
+        let a = true;
+        
+        print(a);
+    }
+    
+    static func main()
+    {
+        let s = BooleanTest();
+        s.booleanTest();
+    }
+}
---
+++ LearningSwift/LearningSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  LearningSwift
+//
+//  Created by wu di on 16/12/31.
+//  Copyright © 2016年 wu di. All rights reserved.
+//
+
+import Foundation
+
+BooleanTest.main();
+
