c07794ac5f1298fa5ddce6dc977478ac0c94d2e7
emacslisp <wudi850620@hotmail.com>
2017-01-06 20:17:26 +1100

    add linux terminal api

---
+++ LinuxSystemAPI/LinuxSystemAPI/LinuxTerminal.cpp
@@ -0,0 +1,9 @@
+//
+//  LinuxTerminal.cpp
+//  LinuxSystemAPI
+//
+//  Created by wu di on 17/1/6.
+//  Copyright © 2017年 wu di. All rights reserved.
+//
+
+#include "LinuxTerminal.hpp"
---
+++ LinuxSystemAPI/LinuxSystemAPI/LinuxTerminal.hpp
@@ -0,0 +1,20 @@
+//
+//  LinuxTerminal.hpp
+//  LinuxSystemAPI
+//
+//  Created by wu di on 17/1/6.
+//  Copyright © 2017年 wu di. All rights reserved.
+//
+
+#ifndef LinuxTerminal_hpp
+#define LinuxTerminal_hpp
+
+#include <stdio.h>
+
+class LinuxTerminal
+{
+public:
+    void cursesLib();
+};
+
+#endif /* LinuxTerminal_hpp */
