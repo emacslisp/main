575a5ec3dabd2aec4c5d57364c6e840a47fd5ff8
emacslisp <java.gdb@gmail.com>
2017-06-25 02:29:40 +1000

    add UICatalog

---
+++ UICatalog/UICatalog/UICatalog/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UICatalog
+//
+//  Created by WuDi on 25/6/17.
+//  Copyright © 2017 WuDi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UICatalog/UICatalog/UICatalog/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UICatalog
+//
+//  Created by WuDi on 25/6/17.
+//  Copyright © 2017 WuDi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
