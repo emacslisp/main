57b8468925fb9ed1baaa808dc3689b29edd69523
emacslisp <java.gdb@gmail.com>
2017-06-27 09:09:24 +1000

    add UICatalog for xcode7

---
+++ xcode7/UICatalog/UICatalog/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UICatalog
+//
+//  Created by eddy.wu on 6/27/17.
+//  Copyright © 2017 eddy.wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ xcode7/UICatalog/UICatalog/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UICatalog
+//
+//  Created by eddy.wu on 6/27/17.
+//  Copyright © 2017 eddy.wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
